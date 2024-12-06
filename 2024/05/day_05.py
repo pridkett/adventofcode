7fbe8189c906cbd4458101f0252487359695d96f
Patrick Wagstrom <160672+pridkett@users.noreply.github.com>
2024-12-05 21:52:50 -0500

    chore(day 5): remove dead code from day 5
    I was trying to be too clever by half on this problem initially. No need
    to be that smart, so I can remove the dead code for flattening the
    rules.
    
    DCO-1.1 Signed-off-by: Patrick Wagstrom <160672+pridkett@users.noreply.github.com>

--- 2024/05/day_05.py
+++ 2024/05/day_05.py
@@ -3,7 +3,6 @@ from typing import List, Dict, Set, Optional
 class Solution:
     def __init__(self):
         self.rules:Dict[int, List[int]] = {}
-        self.flat_rules:Dict[int, List[int]] = {}
 
     def check_ordering(self, ordering: List[int]) -> bool:
         for i in range(len(ordering)):
@@ -32,20 +31,6 @@ class Solution:
         else:
             self.rules[key] = [val]
         return self.rules[key]
-    
-    def _get_recursive_rules_2(self, key: int, visited: Optional[Set[int]] = None) -> Set[int]:
-        if visited is None:
-            visited = set()
-        rv = set(self.rules.get(key, []))
-        visited.add(key)
-        for i in [x for x in rv if x not in visited]:
-            rv = rv.union(self._get_recursive_rules_2(i, visited))
-        return rv
-    
-    def flatten_rules(self) -> Dict[int, List[int]]:
-        for k in self.rules.keys():
-            self.flat_rules[k] = list(self._get_recursive_rules_2(k))
-        return self.flat_rules
         
     def do_all(self, input: str) -> int:
         total_sum = 0
@@ -66,8 +51,6 @@ class Solution:
                 k, v = [int(x) for x in line.split("|")]
                 self.add_rule(k, v)
         
-        self.flatten_rules()
-
         for line in input.splitlines():
             if "," in line:
                 ordering = [int(x) for x in line.split(",")]
@@ -87,4 +70,3 @@ def main(fn: str):
     
 if __name__ == "__main__":
     main("input.txt")
-    
